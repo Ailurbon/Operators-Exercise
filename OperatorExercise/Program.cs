c14c7f5d26fb8391e30d5d068421b1791dc762d3
Marcus Johnson <marx5185@gmail.com>
2025-06-25 03:05:39 -0500

    Created variables that calculate each operation and created a method that calculates the area of a circle once the user gives the radius

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            var add = a + b;
+            var subtract = a - b;
+            var multiply = a * b;
+            var divide = a / b;
+            var modulus = a % b;
+            
+            var quotient = a / b;
+            var remainder = a % b;
+            
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            
+            Console.WriteLine ("What is the radius of the circle");
+            var radius = double.Parse(Console.ReadLine()); 
+            Console.WriteLine( "The area is "+ AreaOfCircle (radius));
+            
+
+        }
+        
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }
